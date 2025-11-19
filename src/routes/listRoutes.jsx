61bffa200fb609e5152c6f2d88d1f102c4773156
laiscoutinho <laiscc575@gmail.com>
2025-11-19 10:54:39 -0300

    feat: creating a basis for the memory game

--- src/routes/listRoutes.jsx
+++ src/routes/listRoutes.jsx
@@ -1,12 +1,14 @@
-import LandingPage from "../pages/LandingPage/LandingPage";
+import LandingPage from "../pages/LandingPage/view";
 import TrailsView from "../pages/Trails/view";
 import DetailTrailView from "../pages/Trails/DetailTrail/view";
 import WatchVideoTrailView from "../pages/Trails/WatchVideoTrail/view";
+import MemoryGameView from "../pages/MemoryGame/view";
 
 export const publicRoutes = [
     { path: '/', element: <LandingPage />, title: 'Início'},
     { path: '/trilhas', element: <TrailsView />, title: 'Trilhas'},
     { path: '/trilhas/:id', element: <DetailTrailView /> },
     { path: '/trilhas/:id/video/:idVideo', element: <WatchVideoTrailView /> },
-    { path: '/historias', element: <LandingPage />, title: 'Histórias'}
+    { path: '/historias', element: <LandingPage />, title: 'Histórias'},
+    { path: '/jogodamemoria', element: <MemoryGameView />, title: 'Jogo da Memória'}
 ];
