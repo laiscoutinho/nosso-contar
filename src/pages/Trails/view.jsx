d639e6bfb094b0e7182a26c0411caf167fd33c26
Rosana Celine <celine.rosana@gmail.com>
2025-11-21 00:18:26 -0300

    style: improve sections style in trail page

--- src/pages/Trails/view.jsx
+++ src/pages/Trails/view.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import { Hand } from "lucide-react";
 
 import trailsData from "../../service/trailsCard.json"
 
 import Logo from "../../assets/logo/logo_Nosso_Contar_wide.png";
+import TrilhasTitle from "../../assets/decor/trilhasTitle.png"
 import iconsHandTitle from "../../assets/decor/trilhasIconsHand.png";
 import trilhasSetas from "../../assets/decor/trilhasInicial.png";
 import TitleEnding from "../../assets/decor/titleEnding.png";
+import TrilhasEnding from "../../assets/decor/trilhasEnding.svg"
 
 import WaveBackground from "../../components/WaveBackground/index";
 import ModuleCard from '../../components/ModuleCard/index';
+import Button from '../../components/Button/index';
 
 // renderizar em amarelinha
 const buildHopscotch = (data) => {
@@ -42,38 +46,22 @@ const TrailsView = () => {
             <main className="relative h-auto ml:h-[80vh] mb-0 px-6 md:px-10 py-12 
                     bg-(--white-soft) 
                     text-start 
-                    rounded-bl-[50px] rounded-br-[50px]
                     flex flex-col items-start justify-center gap-7 md:gap-10 
                     layout-margin
                     overflow-hidden
                 ">
 
-                <div className=" w-full flex items-start space-between gap-6 md:gap-12 flex-nowrap ">
-                    <div className='flex flex-nowrap gap-4'>
+                <div className=" w-full flex items-start space-between gap-6 md:gap-12 flex-nowrap">
+                    <div className='flex flex-col lg:flex-row gap-4'>
                         {/* Título */}
-                        <h2 className="
-                            text-base sm:text-2xl md:text-4xl 
-                            font-bold leading-tight
-                            max-w-full text-left
-                            [text-shadow:0px_0px_4px_rgba(0,0,0,0.25)] 
-                            flex gap-2 justify-center items-center
-                        ">
-                            Bem-Vindo às
-                            <span className="text-(--pink-strong)"> Trilhas </span> do
-                        
-                            {/* Logo */}
-                            <img
-                                src={Logo}
-                                alt="Logo"
-                                className="
-                                    h-6 md:h-11 inline-block -translate-y-1 select-none
+                        <img
+                            src={TrilhasTitle}
+                            alt="Logo"
+                            className="
+                                    max-h-8 sm:max-h-13 lg:max-h-14 inline-block -translate-y-1 select-none
                                 "
-                            />
-                            !
-                        </h2>
-                    </div>
+                        />
 
-                    <div>
                         {/* Ícones */}
                         <img
                             src={iconsHandTitle}
@@ -124,10 +112,31 @@ const TrailsView = () => {
                 </div>
             </main>
 
-            <div className="flex flex-col gap-10" style={{ background: `linear-gradient(to bottom, var(--aqua-light), var(--white-soft))` }}>
+            <div className="
+                    flex 
+                    flex-col 
+                    layout-margin 
+                    overflow-hidden 
+                    rounded-bl-[50px] 
+                    rounded-br-[50px]
+                "
+
+                style={{
+                    background: `
+                        linear-gradient(
+                        to bottom,
+                        var(--white-soft) 0%,
+                        var(--aqua-light) 10%,
+                        var(--aqua-light) 70%,
+                        var(--white-soft) 100%
+                        )
+                    `
+                }}
+            >
+
                 <section className="flex flex-col items-center justify-center relative mt-10">
                     <h2 className="self-start mt-8 mb-4 px-6
-                                    text-base sm:text-xl md:text-2xl md:px-10 font-bold max-w-3xl">
+                                    text-base sm:text-2xl md:text-[2rem] md:px-10 font-bold max-w-3xl">
                         Escolha sua trilha de aprendizagem
                     </h2>
 
@@ -158,12 +167,21 @@ const TrailsView = () => {
 
                 </section>
 
-                <section className="flex flex-col items-center justify-center relative mt-10 gap-5 layout-margin">
-                    <div className="flex flex-col lg:flex-row items-center gap-8 layout-margin mt-4">
-                        <h2 className="self-start text-base sm:text-xl md:text-2xl px-6 md:px-10 font-bold mt-8 mb-4 max-w-3xl">
-                            Cada passo trouxe novas descobertas. Continue explorando e se divertindo com Libras!
-                        </h2>
+                <section className="flex flex-col items-center justify-center relative mt-10 gap-15 pb-25 layout-margin">
+                    <div className="relative w-full max-w-[900px]">
+                        <img
+                            src={TrilhasEnding}
+                            alt=""
+                            className="w-full select-none"
+                        />
                     </div>
+
+                    <Button
+                        onClick={() => navigate("/historias")}
+                        icon={<Hand className="w-4 sm:w-5 h-4 sm:h-5 text-(--white-soft)" />}
+                    >
+                        Ver histórias
+                    </Button>
                 </section>
             </div>
 
