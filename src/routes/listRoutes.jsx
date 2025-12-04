def75a071e9236d93b6962023d5117c4f12abf99
thalytalima211 <thalytalima211@gmail.com>
2025-12-04 14:51:40 -0300

    feat: implements stories landing page

--- src/routes/listRoutes.jsx
+++ src/routes/listRoutes.jsx
@@ -3,12 +3,13 @@ import TrailsView from "../pages/Trails/view";
 import DetailTrailView from "../pages/Trails/DetailTrail/view";
 import WatchVideoTrailView from "../pages/Trails/WatchVideoTrail/view";
 import MemoryGameView from "../pages/MemoryGame/view";
+import StoriesViewPage from "../pages/stories/view";
 
 export const publicRoutes = [
     { path: '/', element: <LandingPage />, title: 'Início'},
     { path: '/trilhas', element: <TrailsView />, title: 'Trilhas'},
     { path: '/trilhas/:id', element: <DetailTrailView /> },
     { path: '/trilhas/:id/video/:idVideo', element: <WatchVideoTrailView /> },
-    { path: '/historias', element: <LandingPage />, title: 'Histórias'},
+    { path: '/historias', element: <StoriesViewPage />, title: 'Histórias'},
     { path: '/jogodamemoria', element: <MemoryGameView />, title: 'Jogo da Memória'}
 ];
